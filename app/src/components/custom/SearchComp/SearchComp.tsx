0ae0d0dfb7485348a414b5b97dec1e9481283b47
mazdak <nazemi.works@gmail.com>
2021-04-12 18:39:50 +0200

    css bug fixed

--- app/src/components/custom/SearchComp/SearchComp.tsx
+++ app/src/components/custom/SearchComp/SearchComp.tsx
@@ -6,28 +6,18 @@ interface IPassingProps {
 }
 
 export const SearchComp: FC<IPassingProps> = ({ countries }) => {
-  const [options, setOptions] = useState<string[] | []>([])
+  const [options, setOptions] = useState<string[]>([])
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setOptions(countries.filter(option => option.includes(e.target.value)))
   }
 
-  // return (
-  //   <div className="App container mt-2 mb-3">
-  //     <h1>Search Bar Dropdown</h1>
-  //     <SearchDropDown options={options} onInputChange={onInputChange} />
-  //   </div>
-  // )
-
   return (
     <div className="m-auto max-w-lg rounded overflow-hidden">
       <p className="p-4">Countries</p>
       <div className="border-b-2 m-0"></div>
-      <p className="p-4">Select Country: </p>
+      <p className="p-4 pb-1">Select Country: </p>
       <SearchDropDown options={options} onInputChange={onInputChange} />
     </div>
   )
 }
-//   <p className="p-4">Countries</p>
-//   <div className="border-b-2 m-0"></div>
-//   <p className="p-4">Select Country: </p>
--- app/src/components/custom/SearchDropDown/SearchDropDown.tsx
+++ app/src/components/custom/SearchDropDown/SearchDropDown.tsx
@@ -5,8 +5,10 @@ interface IPasssingProps {
   options: string[]
 }
 
-export const SearchDropDown: FC<IPasssingProps> = props => {
-  const { options, onInputChange } = props
+export const SearchDropDown: FC<IPasssingProps> = ({
+  options,
+  onInputChange
+}) => {
   const ulRef = useRef(null)
   const inputRef = useRef(null)
 
@@ -16,32 +18,36 @@ export const SearchDropDown: FC<IPasssingProps> = props => {
       (event: ChangeEvent<HTMLInputElement>) => {
         event.stopPropagation()
 
+        ulRef.current.style.display = 'block'
         ulRef.current.style.display = 'flex-row'
 
         onInputChange(event)
       }
     )
+
     document.addEventListener('click', event => {
       ulRef.current.style.display = 'none'
     })
-  }, [])
+  }, [options])
 
   return (
     <div className="relative">
-      <span className="p-2">
-        <input
-          className="m-auto p-3 border-2 rounded h-8 w-11/12"
-          id="search-bar"
-          type="text"
-          placeholder="Search"
-          ref={inputRef}
-          onChange={onInputChange}
-        />
+      <span className="p-1">
+        <div className="relative">
+          <input
+            id="search-bar"
+            type="text"
+            placeholder="Search"
+            ref={inputRef}
+            onChange={onInputChange}
+            className="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500 transition-colors"
+          />
+        </div>
         <br />
       </span>
       <ul
         id="results"
-        className="list-reset mt-2 list-group overflow-y-scroll max-h-72 w-11/12"
+        className="list-reset mt-2 list-group overflow-y-scroll max-h-72 max-w-full"
         ref={ulRef}
       >
         {options.map((option, index) => {
